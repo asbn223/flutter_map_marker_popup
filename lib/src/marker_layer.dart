1634a68a93bcb287700948be317edfc41a7607cb
sabin.nakarmi223 <asbn2231@gmail.com>
2024-05-30 10:55:18 +0545

    Revert

--- lib/src/marker_layer.dart
+++ lib/src/marker_layer.dart
@@ -83,7 +83,11 @@ class _MarkerLayerState extends State<MarkerLayer>
             )) continue;
 
             // Apply map camera to marker position
-            final pos = pxPoint.subtract(map.pixelOrigin);
+            final pos = Point<double>(
+              pxPoint.x - map.pixelOrigin.x,
+              pxPoint.y - map.pixelOrigin.y,
+            );
+            // final pos = pxPoint.subtract(map.pixelOrigin);
 
             var markerChild = m.child;
             if (widget.layerOptions.selectedMarkerBuilder != null &&
